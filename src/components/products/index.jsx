e885c75e38bf77caf01907fa56ea1d20a475dd92
elsacodesid <elsacodesid@gmail.com>
2024-05-17 11:26:10 +0700

    update

---
+++ src/components/products/index.jsx
@@ -0,0 +1,5 @@
+export default function Products({product}){
+    return <div><div className="group flex flex-col items-center border-2 border-blue-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
+        <div className="h-[180px]"><img src={product?.image} alt={product?.title} className="object-cover h-full w-full"/></div>
+        </div></div>
+}
